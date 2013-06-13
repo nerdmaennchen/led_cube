4592a9cc1596687729f186ad0ee9046f196f6adf
Lutz <lutz@fumanoids.de>
2013-06-13 10:00:25 +0200

    simple testing stup for rendering

---
+++ src/application/lutz/surface.c
@@ -0,0 +1,27 @@
+/*
+ * surface.c
+ *
+ *  Created on: May 18, 2013
+ *      Author: lutz
+ */
+
+
+#include <interfaces/cubeRendering.h>
+#include <interfaces/cubeConfig.h>
+
+static void surface_renderingFunction(cubeFrameBuf *buf);
+CUBE_RENDER_FUNCTION(surface_renderingFunction)
+static void surface_renderingFunction(cubeFrameBuf *buf)
+{
+	uint8_t i,j;
+	for (i = 0U; i < CUBE_CONFIG_NUMBER_OF_ROWS; ++i)
+	{
+		for (j = 0U; j < CUBE_CONFIG_NUMBER_OF_ROWS; ++j)
+		{
+			(*buf)[0][i][j] = 1;
+		}
+
+	}
+
+}
+
